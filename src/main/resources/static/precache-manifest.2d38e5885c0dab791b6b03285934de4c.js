9a4e492c3b96477133750cc83dd3e143eb99c318
yuyigong <yuyigong>
2019-01-08 17:35:56 +0800

    配置springCloud

---
+++ src/main/resources/static/precache-manifest.2d38e5885c0dab791b6b03285934de4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "85e2d76cd76e2b0c0dc7",
+    "url": "./static/js/main.85e2d76c.chunk.js"
+  },
+  {
+    "revision": "42b070484f8fe5a21762",
+    "url": "./static/js/1.42b07048.chunk.js"
+  },
+  {
+    "revision": "85e2d76cd76e2b0c0dc7",
+    "url": "./static/css/main.e81b35d8.chunk.css"
+  },
+  {
+    "revision": "42b070484f8fe5a21762",
+    "url": "./static/css/1.e161d5a4.chunk.css"
+  },
+  {
+    "revision": "c828f6dc7de7ae09dfb083d810965d49",
+    "url": "./index.html"
+  }
+];
