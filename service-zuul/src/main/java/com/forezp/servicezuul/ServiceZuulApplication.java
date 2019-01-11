8b01c14ba1d445816751ff6719a2ee14933c4c9f
yuyigong <yuyigong>
2019-01-11 16:55:46 +0800

    配置路由网关zuul

--- service-zuul/src/main/java/com/forezp/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/forezp/servicezuul/ServiceZuulApplication.java
@@ -1,19 +1,19 @@
-package com.forezp.sericefeign;
+package com.forezp.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableFeignClients
-public class SericeFeignApplication {
+@EnableZuulProxy
+public class ServiceZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SericeFeignApplication.class, args);
+        SpringApplication.run(ServiceZuulApplication.class, args);
     }
 
 }
